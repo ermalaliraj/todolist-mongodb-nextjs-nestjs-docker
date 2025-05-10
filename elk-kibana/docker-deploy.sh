a82d8e262a3262023bbcc4f11c9adb59cea1e272
Ermal ALIRAJ <ermal.aliraj@gmail.com>
2025-05-10 13:43:12 +0200

    jenkins kibana deploy

---
+++ elk-kibana/docker-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME="kibana"
+IMAGE_VERSION="7.17.9"
+
+echo "Stopping mongo container..."
+docker stop ${IMAGE_NAME} || true
+echo "Removing ${IMAGE_NAME} container..."
+docker rm ${IMAGE_NAME} || true
+echo "Removing ${IMAGE_NAME} image..."
+docker rmi ${IMAGE_NAME}:${IMAGE_VERSION} || true
+
+echo "Building new image..."
+docker compose up -d
