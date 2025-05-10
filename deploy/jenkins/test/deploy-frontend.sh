79847b95816777ae2a935ca5f98d6164068fcc26
Ermal ALIRAJ <ermal.aliraj@gmail.com>
2025-05-10 13:10:01 +0200

    docker for frontend

--- deploy/jenkins/test/deploy-frontend.sh
+++ deploy/jenkins/test/deploy-frontend.sh
@@ -10,7 +10,8 @@ ZIP_NAME=todolist-frontend.tar.gz
 SCRIPT_INSTALL=install-test.sh
 
 # Exit if change is outside the interested project
-BUILD_LOG_PATH="/var/lib/jenkins/jobs/todolist/jobs/$SRC_DIR/builds/$BUILD_NUMBER/log"
+JOB_NAME_SHORT=$(echo "$JOB_NAME" | cut -d'/' -f1) # extracts the main project name
+BUILD_LOG_PATH="/var/lib/jenkins/jobs/${JOB_NAME_SHORT}/jobs/$SRC_DIR/builds/$BUILD_NUMBER/log"
 echo "BUILD_LOG_PATH: $BUILD_LOG_PATH"
 BUILD_CAUSE=$(grep -i "Started by" "$BUILD_LOG_PATH" | head -n 1) # Searching this: "Started by user Ermal as frontend", to force build if Run fired by user and not by Push
 CHANGED_FILES=$(git diff --name-only HEAD^1)
