164479736980383ecb2cb6523605f5640eedd838
Ermal ALIRAJ <ermal.aliraj@gmail.com>
2025-05-11 19:31:08 +0200

    port correction

--- backend/install.sh
+++
@@ -1,5 +0,0 @@
-npm install -f
-npm run build-test
-
-pm2 delete todolist-backend:5041
-pm2 start npm --name "todolist-backend:5041" -- start --attach
--- frontend/src/navigation/vertical/index.js
+++ frontend/src/navigation/vertical/index.js
@@ -1,19 +1,14 @@
 const navigation = () => [
   {
-    title: 'Dashboard',
-    path: '/dashboard',
-    icon: 'mdi:home-outline'
+    title: 'TodoList',
+    icon: 'mdi:work',
+    path: '/todolist'
   },
   {
     title: 'Merchants',
     icon: 'mdi:handshake',
     path: '/merchants'
   },
-  {
-    title: 'TodoList',
-    icon: 'mdi:work',
-    path: '/todolist'
-  }
 ]
 
 export default navigation
